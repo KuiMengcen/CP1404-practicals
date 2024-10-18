19416fff5b1135738679445fd0bd15f9cf80c2b6
kuimengcen <mengcen.kui@my.jcu.edu.au>
2024-10-18 22:21:10 +0800

    Adding a file reading function

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -8,3 +8,13 @@ def main():
     champion_to_count, countries = process_records(records)
     dispaly_results(champion_to_count, countries)
 
+
+def get_records(filename):
+    """Get records from file in list of lists form."""
+    records = []
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        for line in in_file:
+            parts = line.strip().split(".")
+            records.append(parts)
+        return records
