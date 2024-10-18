96c1d786b084cbc47b21d11324e0a6d9f549d027
kuimengcen <mengcen.kui@my.jcu.edu.au>
2024-10-18 22:03:22 +0800

    Execute the get_name function to extract a name from an email message

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -8,3 +8,10 @@ def main():
         while SPECIAL_CHARACTER not in email:
             print("Error")
             email = input("Enter your email(Must contain @): ")
+
+def get_name(email):
+    "Extract expected name from email address."
+    prefix = email.split('@')[0]
+    parts = prefix.split('.')
+    name = " ".join(parts).title()
+    return name
