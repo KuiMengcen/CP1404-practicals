9d451c5ca54b3a9e03fab494e91f5cb3cb2856c5
kuimengcen <mengcen.kui@my.jcu.edu.au>
2024-11-03 05:55:31 +0800

    Modify code

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,7 +1,6 @@
 CURRENT_YEAR = 2024
 VINTAGE_YEAR = 50
 
-
 class Guitar:
 
     def __init__(self, name="", year=0, cost=0):
@@ -12,9 +11,12 @@ class Guitar:
     def __str__(self):
         return f"{self.name}, ({self.year}) : ${self.cost:,.2f}"
 
-
     def get_age(self):
         return CURRENT_YEAR - self.year
 
     def is_vintage(self):
         return self.get_age() >= VINTAGE_YEAR
+
+    def __lt__(self, other):
+        """Less than, used for sorting Guitars - by year released."""
+        return self.year < other.year
