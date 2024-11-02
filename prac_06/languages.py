48d6e6ae6cdc96d91a746020ca315e704acf5dd5
kuimengcen <mengcen.kui@my.jcu.edu.au>
2024-11-03 05:48:09 +0800

    Keeps a list of all instances

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,8 +1,11 @@
 from programming_language import ProgrammingLanguage
 
+
 def main():
 
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual basic", "Static", False, 1991)
     print(python)
+
+    languages = [python, ruby, visual_basic]
