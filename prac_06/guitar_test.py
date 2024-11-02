0928ffd9b184c9b41e919470b4fdd9eb76402920
kuimengcen <mengcen.kui@my.jcu.edu.au>
2024-11-03 05:58:22 +0800

    Calculation of expected age

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,1 +1,10 @@
 from prac_06.guitar import Guitar
+
+
+def run_test():
+    name = "Gibson L-5 CRS"
+    year = 1922
+    cost = 16035.48
+
+    guitar_1 = Guitar(name, year, cost)
+    guitar_2 = Guitar("Another Guitar", 2012, 1512.9)
