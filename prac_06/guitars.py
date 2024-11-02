cd7feb765b296444a3a7537a099f721d5e3da227
kuimengcen <mengcen.kui@my.jcu.edu.au>
2024-11-03 06:01:32 +0800

    Sort guitars by year

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,5 +1,6 @@
 from prac_06.guitar import Guitar
 
+
 def main():
     guitars = []
 
@@ -17,3 +18,15 @@ def main():
     guitars.append(Guitar("Gibson L-5 CES", 1992, 16035.40))
     guitars.append(Guitar("Frender Sratocruisers", 2014, 765.4))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    if guitars:
+        guitars.sort()
+        print("These are my guitars:")
+
+        for i, guitar in enumerate(guitars, 1):
+            vintage_string = ""
+            if guitar.is_vintage():
+                vintage_string = " (vintage)"
+            print("Guitar {0}: {1.name:<23} ({1.year:<4}), worth ${1.cost:10,.2f}").format(i, guitar, vintage_string)
+        else:
+            print("No guitars :( Quick, go and buy one!")
