0fbf0e3d57c4b642eb5e8eba06553c60113f8930
kuimengcen <mengcen.kui@my.jcu.edu.au>
2024-11-10 09:52:52 +0800

    Initializes a new instance of the Guitar class

---
+++ prac_07/myguitars.py
@@ -0,0 +1,7 @@
+class Guitar:
+    def __init__(self, guitar_name="", guitar_year=0, guitar_cost=0):
+        self.name = guitar_name
+        self.year = guitar_year
+        self.cost = guitar_cost
+
+
