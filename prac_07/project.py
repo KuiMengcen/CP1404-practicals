bf58a54838652bb42d87a389de4fcf527ec74a68
kuimengcen <mengcen.kui@my.jcu.edu.au>
2024-11-10 09:39:49 +0800

    String representation of the Project

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,5 +1,6 @@
 import datetime
 
+
 class Project:
     def __init__(self, name="", start_date="", priority=0, cost_estimate=0, completion_percentage=0):
         self.name = name
@@ -8,3 +9,7 @@ class Project:
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        return f"{self.name}, start: {self.start_date_str}, priority {self.priority}, " \
+               f"estimate: ${self.cost_estimate}, completion: {self.completion_percentage}%"
