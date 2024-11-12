2bc59a939356a7c324c690f81ad806b07fdf5f2f
kuimengcen <mengcen.kui@my.jcu.edu.au>
2024-11-13 06:53:31 +0800

    Sets the window title

--- prac_08/KivyDemos-master/dynamic_labels.py
+++ prac_08/KivyDemos-master/dynamic_labels.py
@@ -8,3 +8,9 @@ class DynamicLabelsApp(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.name_to_phone = {"Bob Brown": "0414144411", "Cat Cyan": "0441411211", "Oren Ochre": "0432123456"}
+
+    def build(self):
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_labels()
+        return self.root
