f4c1cb3a2c1172970d7f9a8766059f2c88cdddcf
kuimengcen <mengcen.kui@my.jcu.edu.au>
2024-11-13 06:52:51 +0800

    Initialize the DynamicLabelsApp

--- prac_08/KivyDemos-master/dynamic_labels.py
+++ prac_08/KivyDemos-master/dynamic_labels.py
@@ -1,3 +1,10 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.label import Label
+
+
+class DynamicLabelsApp(App):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.name_to_phone = {"Bob Brown": "0414144411", "Cat Cyan": "0441411211", "Oren Ochre": "0432123456"}
