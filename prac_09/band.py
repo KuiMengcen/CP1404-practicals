16f14e20d55f15f3183d06c0e5018f0caf23f74c
kuimengcen <mengcen.kui@my.jcu.edu.au>
2024-11-24 15:40:03 +0800

    Method to add a band to the class-wide list of bands

--- prac_09/band.py
+++ prac_09/band.py
@@ -7,3 +7,6 @@ class Band:
     def __str__(self):
         band_list = ', '.join(str(i) for i in self.bands)
         return f"{self.name}({band_list})"
+
+    def add(self, band):
+        self.bands.append(band)
