3ca1d477f29d1c18bcc1f9924ce1fdd8919bf343
kuimengcen <mengcen.kui@my.jcu.edu.au>
2024-11-24 15:39:45 +0800

    String representation method for the Band class

--- prac_09/band.py
+++ prac_09/band.py
@@ -3,3 +3,7 @@ class Band:
 
     def __init__(self, name):
         self.name = name
+
+    def __str__(self):
+        band_list = ', '.join(str(i) for i in self.bands)
+        return f"{self.name}({band_list})"
