7da990776da3b469db8e6977fbf8d8d121ae505a
kuimengcen <mengcen.kui@my.jcu.edu.au>
2024-11-24 15:41:51 +0800

    import module

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,4 @@
+from prac_09.silver_service_taxi import SilverServiceTaxi
+from prac_09.taxi import Taxi
+
+taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
