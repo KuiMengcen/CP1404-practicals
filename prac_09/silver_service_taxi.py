1870d17885558549853659c64912b53aa0605240
kuimengcen <mengcen.kui@my.jcu.edu.au>
2024-11-24 15:36:22 +0800

    Constructor for the SilverServiceTaxi class

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -3,3 +3,7 @@ from prac_09.taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
     flagfall = 4.50
+
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.price_per_km *= fanciness
