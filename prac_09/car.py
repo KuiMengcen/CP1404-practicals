f1be78ce786e2cba177ede837e55167b5e5dc5cc
kuimengcen <mengcen.kui@my.jcu.edu.au>
2024-11-24 15:27:29 +0800

    pecialised version of a Car that includes fare costs

---
+++ prac_09/car.py
@@ -0,0 +1,18 @@
+"""
+CP1404/CP5632 Practical
+Car class
+"""
+from prac_09.car import Car
+
+"""
+CP1404/CP5632 Practical
+Car class
+"""
+from prac_09.car import Car
+
+
+class Taxi(Car):
+    """Specialised version of a Car that includes fare costs."""
+    price_per_km = 1.23
+
+
