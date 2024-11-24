7e755bbe955c3c1fbbd752f9e55ca1d047479887
kuimengcen <mengcen.kui@my.jcu.edu.au>
2024-11-24 15:42:27 +0800

    Main program loop

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -2,3 +2,29 @@ from prac_09.silver_service_taxi import SilverServiceTaxi
 from prac_09.taxi import Taxi
 
 taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+
+def main():
+    current_taxi = None
+    total_bill = 0
+
+    print("Let's drive!")
+    print("q)uit, c)hoose taxi, d)rive")
+    user_choice = input(">>> ").upper()
+    user_choice = check_input(total_bill, user_choice)
+
+    while user_choice != "Q":
+        if user_choice == "C":
+            current_taxi = choose_taxi()
+
+        if user_choice == "D":
+            total_bill = drive_car(current_taxi, total_bill)
+
+        print(f"Bill to date: ${total_bill:.2f}")
+        print("q)uit, c)hoose taxi, d)rive")
+        user_choice = input(">>> ").upper()
+        user_choice = check_input(total_bill, user_choice)
+
+    print(f"Total trip cost: ${total_bill}")
+    print("Taxis are now:")
+    for i, j in zip(taxis, range(0, len(taxis))):
+        print(f"{j} - {i}")
