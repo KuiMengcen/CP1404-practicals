25777761071ce0863db92a9bb93c88eed8071a5b
kuimengcen <mengcen.kui@my.jcu.edu.au>
2024-11-30 00:02:22 +0800

    First time getting user input

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -7,3 +7,10 @@ warnings.filterwarnings("ignore", category=UserWarning, module="wikipedia")
 def get_user_input(prompt="Enter page title: "):
     """Get user input"""
     return input(prompt)
+
+def main():
+    print("Welcome to the Wikipedia Search Tool!")
+    # First time getting user input
+    title = get_user_input()
+    while title:
+        try:
