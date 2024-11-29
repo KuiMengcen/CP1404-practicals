f83e3301a99efe8e41e7f557c3e0b5762a6d08fc
kuimengcen <mengcen.kui@my.jcu.edu.au>
2024-11-29 23:37:48 +0800

    Modify bugs

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -1,6 +1,6 @@
 """
 CP1404/CP5632 Practical
-Testing code using assert and doctest
+Testing demo using assert and doctest
 """
 
 import doctest
@@ -22,7 +22,7 @@ def is_long_word(word, length=5):
     >>> is_long_word("Python", 6)
     True
     """
-    return len(word) > length
+    return len(word) >= length
 
 
 def run_tests():
@@ -38,15 +38,15 @@ def run_tests():
     # assert test with custom message,
     # used to see if Car's init method sets the odometer correctly
     # this should pass (no output)
-    car = Car()
-    assert car._odometer == 0, "Car does not set odometer correctly"
+    test_car = Car()
+    assert test_car._odometer == 0, "Car does not set odometer correctly"
 
     # TODO: 2. write assert statements to show if Car sets the fuel correctly
     # Note that Car's __init__ function sets the fuel in one of two ways:
     # using the value passed in or the default
     # You should test both of these
-    car = Car(fuel=10)
-    assert teat_car.fule > 0 "Car does not set odometer correctly"
+    test_car = Car(fuel=10)
+    assert test_car.fuel > 0
 
 
 run_tests()
@@ -56,24 +56,26 @@ run_tests()
 doctest.testmod()
 
 # TODO: 4. Fix the failing is_long_word function
-# (Don't change the tests, change the function!)
+# (don't change the tests, change the function!)
 
 # TODO: 5. Write and test a function to format a phrase as a sentence,
 # starting with a capital and ending with a single full stop.
 # Important: start with a function header and just use pass as the body
 # then add doctests for 3 tests:
-#   'hello' -> 'Hello.'
-#   'It is an ex parrot.' -> 'It is an ex parrot.'
-# and one more that you decide is a useful test.
-# Run your doctests and watch the tests fail.
-# Then write the body of the function so that the tests pass.
+# 'hello' -> 'Hello.'
+# 'It is an ex parrot.' -> 'It is an ex parrot.'
+# and one more you decide (one that is valid!)
+# test this and watch the tests fail
+# then write the body of the function so that the tests pass
+
 
 def format_phrase(words):
     if words[0].islower():
         words = words[0].upper() + words[1:]
-        if words[-1] != ".":
-            words += "."
-        return words
+    if words[-1] != ".":
+        words += "."
+    return words
+
 
 assert format_phrase('hello') == 'Hello.'
 
