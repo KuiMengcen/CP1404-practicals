4fd081a6a47b9980c62ac5891f696d13ede718e7
kuimengcen <mengcen.kui@my.jcu.edu.au>
2024-11-30 19:27:52 +0800

    Get user input

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -3,3 +3,8 @@ import warnings
 
 # Ignore BeautifulSoup warnings
 warnings.filterwarnings("ignore", category=UserWarning, module="wikipedia")
+
+
+def get_user_input(prompt="Enter page title: "):
+    """Getting user input"""
+    return input(prompt)
